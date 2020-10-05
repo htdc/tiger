602fc6d9abe2df250768d68172f9563cd8081888
Martin Feckie <mfeckie@gmail.com>
2020-10-05 19:22:14 +0800

    Support the rest of the charge API

--- lib/tiger/charge.ex
+++ lib/tiger/charge.ex
@@ -1,9 +1,47 @@
 defmodule Tiger.Charge do
+  @moduledoc """
+  Used for interacting with Charges
+  """
   alias Tesla.Client
   alias Tiger.Structs.CreateCharge
 
+  @doc """
+  Make a charge
+  """
   def create(%Client{} = client, %CreateCharge{} = params) do
-    Tesla.post(client, "charges", FormSafe.encode(params))
+    Tesla.post(client, endpoint_path(), FormSafe.encode(params))
   end
 
+  def create(%Client{} = client, params) when is_map(params) do
+    Tesla.post(client, endpoint_path(), FormSafe.encode(params))
+  end
+
+  @doc """
+  Retrieve a charge
+  """
+  def get(%Client{} = client, id) when is_binary(id) do
+    Tesla.get(client, "#{endpoint_path()}/#{id}")
+  end
+
+  @doc """
+  Update a charge
+  """
+  def update(%Client{} = client, id, params) when is_map(params) do
+    Tesla.post(client, "#{endpoint_path()}/#{id}", FormSafe.encode(params))
+  end
+
+  @doc """
+  List charges
+
+  ## Parameters
+
+  Passed as a keyword list e.g. `[order: "desc"]`
+  """
+  def list(%Client{} = client, query \\ []) do
+    Tesla.get(client, endpoint_path(), query: query)
+  end
+
+  defp endpoint_path do
+    "charges"
+  end
 end
--- test/tiger/charge_test.exs
+++ test/tiger/charge_test.exs
@@ -1,8 +1,7 @@
 defmodule Tiger.ChargeTest do
   use ExUnit.Case, async: true
   alias Tiger.Client
-  alias Tiger.Charge
-  alias Tiger.Structs.CreateCharge
+  alias Tiger.Structs.{CreateCharge, Charge}
 
   use Nug,
     upstream_url: Client.default_url(),
@@ -10,11 +9,84 @@ defmodule Tiger.ChargeTest do
 
   test "Can make a charge" do
     with_proxy("create_charge.fixture") do
-      {:ok, charge} =
-        Charge.create(client, %CreateCharge{
+      {:ok, %Tesla.Env{body: %Charge{} = charge}} =
+        Tiger.Charge.create(client, %CreateCharge{
           amount: 2000,
-          source: "tok_amex"
+          source: "tok_au"
         })
+
+      assert charge.id =~ ~r/ch_/
+      assert charge.captured
+    end
+  end
+
+  test "Can make a charge with Connect" do
+    with_proxy("create_charge_connect.fixture") do
+      {:ok, %Tesla.Env{body: %Charge{} = charge}} =
+        Tiger.Charge.create(client, %CreateCharge{
+          amount: 2000,
+          source: "tok_au",
+          application_fee: 250,
+          transfer_data: %{
+            destination: "acct_1HSZKXFFwAeTesFQ"
+          }
+        })
+
+      assert charge.transfer.destination == "acct_1HSZKXFFwAeTesFQ"
+      assert charge.transfer.destination_payment =~ ~r/py_/
+      assert charge.transfer.amount == 2000
+      assert charge.application_fee_amount == 250
+    end
+  end
+
+  test "Can make a pre-auth charge" do
+    with_proxy("create_charge_pre_auth.fixture") do
+      {:ok, %Tesla.Env{body: %Charge{} = charge}} =
+        Tiger.Charge.create(client, %CreateCharge{
+          amount: 2000,
+          source: "tok_au",
+          capture: false
+        })
+
+      assert charge.id =~ ~r/ch_/
+      refute charge.captured
+    end
+  end
+
+  test "Can make a retrieve a charge" do
+    with_proxy("get_charge.fixture") do
+      {:ok, %Tesla.Env{body: %Charge{} = charge}} =
+        Tiger.Charge.get(client, "ch_1HYr2GClStGc5XwOogX26olX")
+
+      assert charge.id == "ch_1HYr2GClStGc5XwOogX26olX"
+      assert charge.captured
+    end
+  end
+
+  test "Can make a update a charge" do
+    with_proxy("update_charge.fixture") do
+      {:ok, %Tesla.Env{body: %Charge{} = charge}} =
+        Tiger.Charge.update(client, "ch_1HYr2FClStGc5XwOTkbd7AuG", %{description: "I am updated"})
+
+      assert charge.description == "I am updated"
+    end
+  end
+
+  test "Can retrieve a list of charges" do
+    with_proxy("list_charges.fixture") do
+      {:ok, %Tesla.Env{body: charges}} = Tiger.Charge.list(client)
+      [%Charge{} = first | _] = charges
+
+      assert first.amount == 2000
+      assert length(charges) == 10
+    end
+  end
+
+  test "Can retrieve a list of charges with query parameters" do
+    with_proxy("list_charges_with_query.fixture") do
+      {:ok, %Tesla.Env{body: charges}} = Tiger.Charge.list(client, limit: 100)
+
+      assert length(charges) == 100
     end
   end
 end
