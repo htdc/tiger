5be386a695bc581621b1ea70764d5f557fa2891c
Martin Feckie <mfeckie@gmail.com>
2020-10-05 18:30:16 +0800

    Add test charge builder

---
+++ test/support/test_client.ex
@@ -0,0 +1,24 @@
+defmodule TestClient do
+  @moduledoc """
+  Helper functions for use during testing
+  """
+
+  @doc """
+  Convenience function for building a test environment client
+  """
+  def setup(address) do
+    Process.put(:client_url, address)
+
+    Tiger.Client.new(config(), address)
+  end
+
+  @doc """
+  Read config from system environment variables
+  """
+  def config do
+    %Tiger.Config{
+      api_key: System.get_env("STRIPE_TEST_API_KEY")
+    }
+  end
+
+end
